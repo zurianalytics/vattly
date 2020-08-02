7e989efd57fef445bf7299008b634efe253b44dd
Slavin Donchev <zuri-analytics@protonmail.com>
2020-08-02 17:17:47 +0200

    Finished product

--- src/js/json-renderer.js
+++ src/js/json-renderer.js
@@ -2,6 +2,9 @@ import JSONFormatter from 'json-formatter-js'
 
 document.querySelectorAll('code.json').forEach(el => {
     let renderedJson = new JSONFormatter(JSON.parse(el.innerHTML)).render();
+    renderedJson
+        .querySelectorAll(".json-formatter-boolean")
+        .forEach(e => e.innerText == 'true' ? e.classList.add('true') : e.classList.add('false'))
     var parent = el.parentNode
     parent.removeChild(el)
     parent.appendChild(renderedJson)
--- src/js/section-introduction.js
+++ src/js/section-introduction.js
@@ -1,5 +1,7 @@
 import {env} from './global-variables'
 import axios from 'axios'
+import JSONFormatter from 'json-formatter-js'
+import { CountUp } from 'countup.js';
 
 new Vue({
 
@@ -13,16 +15,34 @@ new Vue({
             vatError: null,
             server: env.api + "/validate/",
             apiName: env.name,
-            url: this.server + this.vat
+            url: this.server + this.vat,
+            speed: 90
         }
     },
 
 
+    mounted: function()
+    {
+        this.getVat();
+        setInterval(this.lastMSSpeed.bind(this), 2000);
+    },
 
     methods:
         {
+            lastMSSpeed: function()
+            {
+
+                // Get fund
+                axios
+                    .get(env.api + "/speed")
+                    .then(response => { 
+                        new CountUp('count-up', response.data, {duration: 2, startVal: this.speed}).start();
+                        this.speed = response.data 
+                    })
+            },
+
             getVat: function () {
-                this.url = this.server + this.isin;
+                this.url = this.server + this.vat;
 
                 console.info("Loading vat check on: " + this.url);
 
@@ -30,29 +50,27 @@ new Vue({
                 axios
                     .get(this.url)
                     .then(response => {
-                        console.log("dadw")
                         this.vatError = false
                         this.vatData = response.data
 
-                        document.querySelector('#demo-content').forEach(el => {
-                            console.log("dadw")
-                            let renderedJson = new JSONFormatter(this.vatData).render();
-                            var parent = el.parentNode
-                            parent.removeChild(el)
-                            document.querySelector('#demo-content').appendChild(renderedJson)
-                        })
-
+                        let container = document.querySelector('#demo-content')
+                        let renderedJson = new JSONFormatter(this.vatData).render()
+                        // Make Boolean Values Green / Red
+                        renderedJson
+                            .querySelectorAll(".json-formatter-boolean")
+                            .forEach(e => e.innerText == 'true' ? e.classList.add('true') : e.classList.add('false'))
+                        container.removeChild(container.firstChild)
+                        container.appendChild(renderedJson)
                     })
                     // Free requests have expired
                     .catch(error => {
                         console.error(error)
-                        //this.fundError = "Unspecified error occured."
                         if (typeof error.response === "undefined")
-                            this.fundError = "Unspecified error occured."
+                            this.vatError = "Unspecified error occured."
                         else if (error && error.response.status == 404)
-                            this.fundError = "Unfortunatelly the page provided could not be found. Did you try with a correct VAT?"
+                            this.vatError = "Unfortunatelly the page provided could not be found. Did you try with a correct VAT?"
                         else if (error && error.response.status == 401)
-                            this.fundError = "Unfortunatelly your free requests have expired."+ 
+                            this.vatError = "Unfortunatelly your free requests have expired."+ 
                             " Please feel free to visit again tomorrow, or subscribe to a plan."
                     })
             },
