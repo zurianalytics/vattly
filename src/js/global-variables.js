0679cd3f2a4ff9ef4a8d8d82bda66ded5117d150
Slavin Donchev <zuri-analytics@protonmail.com>
2020-08-04 16:37:22 +0200

    Enabled Production Mode

--- src/js/global-variables.js
+++ src/js/global-variables.js
@@ -3,10 +3,10 @@ module.exports =
     env:
     {
         //api: "https://api.etf-data.com",
-        api: "https://test.api.vattly.com",
+        api: "https://api.vattly.com",
         website: "https://vattly.com/",
         //website: "http://localhost:8080",
         name: "Vattly",
-        stripeKey: "pk_test_Dzl7yTz2o6HP1UYa4Wb4ZC8g00tZaM2vr5"
+        stripeKey: "pk_live_Ku4juyvaWDrHh3DQHg55aFaI00pj25URBI"
     }
 }
